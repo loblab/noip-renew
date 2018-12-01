1b9f5a9ef7b9e64286621d879ba2aba7b390c6e6
Gabriele <neothematrix@users.noreply.github.com>
2018-12-01 15:15:50 +0100

    added NUM_HOSTS parameter

--- noip-renew.sh
+++ noip-renew.sh
@@ -2,13 +2,14 @@
 
 USERNAME="change-the-username"
 PASSWORD="change-the-password"
+NUM_HOSTS="3" # make sure to change this to the number of configured dynamic hosts on your no-ip account
 
 LOGDIR=$1
 
 if [ -z "$LOGDIR" ]; then
-    ./noip-renew.py "$USERNAME" "$PASSWORD" 2
+    ./noip-renew.py "$USERNAME" "$PASSWORD" "$NUM_HOSTS" 2
 else
     cd $LOGDIR
-    noip-renew.py "$USERNAME" "$PASSWORD" 0 >> log
+    noip-renew.py "$USERNAME" "$PASSWORD" "$NUM_HOSTS" 0 >> log
 fi
 
