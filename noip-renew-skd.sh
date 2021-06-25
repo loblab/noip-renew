aeb75b3cead5a44227fb3a8a069a64418188f7ba
Alberto <39909668+albertomuve@users.noreply.github.com>
2021-06-25 20:15:20 +0200

    Update noip-renew-skd.sh
    If you dont put the .sh it won't work. Put this on the cron:
    30 0 * * *   root    /usr/local/bin/noip-renew-root /var/log/noip-renew/root
    
    vs the right way:
    30 0 * * *   root    /usr/local/bin/noip-renew-root.sh /var/log/noip-renew/root

--- noip-renew-skd.sh
+++ noip-renew-skd.sh
@@ -4,8 +4,8 @@ SUDO=sudo
 LOGDIR=/var/log/noip-renew/$USER
 INSTDIR=/usr/local/bin
 INSTEXE=$INSTDIR/noip-renew-$USER
-CRONJOB="30 0 * * *   $USER    $INSTEXE $LOGDIR"
-NEWCJOB="30 0 $1 $2 *   $USER    $INSTEXE $LOGDIR"
+CRONJOB="30 0 * * *   $USER    ${INSTEXE}.sh $LOGDIR"
+NEWCJOB="30 0 $1 $2 *   $USER    ${INSTEXE}.sh $LOGDIR"
 $SUDO crontab -u $USER -l | grep -v '/noip-renew*'  | $SUDO crontab -u $USER -
 if [[ $3 = "True" ]]; then
     ($SUDO crontab -u $USER -l; echo "$NEWCJOB") | $SUDO crontab -u $USER -
