fd8c903e9b772e767b775958a322fdb810355eff
Gabriele <neothematrix@users.noreply.github.com>
2021-08-13 07:52:39 +0200

    updated dynamic-dns page url
    fixes #80

--- noip-renew.py
+++ noip-renew.py
@@ -39,7 +39,7 @@ class Robot:
 
     USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:64.0) Gecko/20100101 Firefox/64.0"
     LOGIN_URL = "https://www.noip.com/login"
-    HOST_URL = "https://my.noip.com/#!/dynamic-dns"
+    HOST_URL = "https://my.noip.com/dynamic-dns"
 
     def __init__(self, username, password, debug):
         self.debug = debug
